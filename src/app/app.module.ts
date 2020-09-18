3c147c0c9d7759e8efcc7a167d34be6375a8d0ba
chintan <chintansutariya322@gmail.com>
2020-09-18 13:33:46 +0530

    App module and app component cleared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { MatVideoModule } from 'mat-video';
 import { VideoComponent } from './video/video.component';
 
 import { VgCoreModule } from '@videogular/ngx-videogular/core';
@@ -30,16 +21,6 @@ import { BookmarkModelComponent } from './video/bookmark-model/bookmark-model.co
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    MatFormFieldModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
-    MatSlideToggleModule,
-    MatTooltipModule,
-    MatRadioModule,
-    MatVideoModule,
     VgCoreModule,
     VgControlsModule,
     VgOverlayPlayModule,
